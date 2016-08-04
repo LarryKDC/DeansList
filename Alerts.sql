1282a469f26318c4509d2d6e3934ffbe729a8cca
LarryKDC <LarryKDC@users.noreply.github.com>
2016-08-04 11:09:29 -0400

    update medical alert
    include medical alerts where the expiration date is null

--- Alerts.sql
+++ Alerts.sql
@@ -21,10 +21,11 @@ union all
 select 
 student_number,
 'Medical' "Alert Type",
-alert_medical "Alert Text"
+alert_medical "Alert Text",
+alert_medicalexpires
 from PSSIS_Student_Alerts
 where alert_medical is not null
-and (alert_medicalexpires>=sysdate or alert_medicalexpires='01-JAN-1900')
+and (alert_medicalexpires>=sysdate or alert_medicalexpires='01-JAN-1900' or alert_medicalexpires is null)
 
 union all
 
